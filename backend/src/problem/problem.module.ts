688e852d3f13f267e52fd6b940583cd12e99f49a
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2025-05-07 11:08:23 +0530

    feat(backend) - added GET: /problems API

---
+++ backend/src/problem/problem.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';  // Import the NestJS Module decorator
+import { ProblemService } from './problem.service';  // Import the service
+import { ProblemController } from './problem.controller';  // Import the controller
+import { PrismaService } from '../prisma/prisma.service';  // Import PrismaService to be injected
+
+@Module({
+  imports: [],
+  controllers: [ProblemController],  // Register the controller
+  providers: [ProblemService, PrismaService],  // Register the service and Prisma service as providers
+})
+export class ProblemModule {}
