cdec895d1ea50ed1615b258046e6976f0a206285
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2025-08-27 12:27:28 +0530

    backend - added db checkup for /api/v1/health/readiness

--- backend/src/health/health.module.ts
+++ backend/src/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [HealthController],
-  providers: [HealthService],
+  providers: [HealthService, PrismaService],
 })
 export class HealthModule {}
